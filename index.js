57c6cf38351b9778f031995d19243e83271e1b83
Frejus MONTCHI <montchifrejussss@gmail.com>
2026-02-09 13:00:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const boutonlike=document.querySelector(".btnlike");
-const sonLike=new Audio("https://www.soundjay.com/buttons/sounds/button-16.mp3");
+const sonLike=new Audio("https://cdn.pixabay.com/download/audio/2025/01/19/audio_af5c830f07.mp3?filename=freesoundeffects-button-click-289742.mp3");
 boutonlike.addEventListener("click", ()=>
 {   sonLike.currentTime=0;
     sonLike.play();
